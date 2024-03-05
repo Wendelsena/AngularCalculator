f3856972eaee7384be013cacf8a4361f691a53b7
Wendel <wendel4019@gmail.com>
2024-03-05 19:40:32 -0300

    Created form-calc.component

---
+++ src/app/form-calc/form-calc.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-calc',
+  templateUrl: './form-calc.component.html',
+  styleUrl: './form-calc.component.css'
+})
+export class FormCalcComponent {
+
+}
