490f41d5aa3f8a9480c4006d3ba88b6f199ab797
Wendel <wendel4019@gmail.com>
2024-03-06 14:15:46 -0300

    Add basic arithmetic operations

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -10,10 +10,28 @@ export class FormCalcComponent {
   num1: number = 0;
   num2: number = 0;
   sum: number = 0;
+  difference: number = 0;
+  product: number = 0;
+  Quotient: number = 0;
 
   /*Método de soma*/
   onClickSum() {
     this.sum = this.num1 + this.num2;
+  }
+
+   /*Método de subtração*/
+  onClickDifference() {
+    this.sum = this.num1 - this.num2;
+  }
+
+   /*Método de multiplicação*/
+  onClickProduct () {
+    this.sum = this.num1 * this.num2;
+  }
+
+   /*Método de divisão*/
+  onClickQuotient() {
+    this.sum = this.num1 / this.num2;
   }
 
 }
