743886f6e98f9e232371efc99b433c147e171c49
Wendel <wendel4019@gmail.com>
2024-03-06 15:37:28 -0300

    Swith-Case

--- src/app/form-calc/form-calc.component.ts
+++ src/app/form-calc/form-calc.component.ts
@@ -8,30 +8,38 @@ import { Component } from '@angular/core';
 export class FormCalcComponent {
   num1: number = 0;
   num2: number = 0;
-  equal: number = 0;
-
-  onClickSum(): void {
-    this.equal = this.num1 + this.num2;
-  }
-
-  onClickDifference(): void {
-    this.equal = this.num1 - this.num2;
-  }
-
-  onClickProduct(): void {
-    this.equal = this.num1 * this.num2;
-  }
-
-  onClickQuotient(): void {
-    if (this.num2 !== 0) {
-      this.equal = this.num1 / this.num2;
+  equal: any;
+  selectedOperator: any;
+  
+  onClickEqual(): void {
+    // Verifica se os números foram fornecidos antes de calcular o resultado
+    if (this.num1 !== null && this.num2 !== null) {
+      // Realiza a operação matemática
+      switch (this.selectedOperator) {
+        case '+':
+          this.equal = this.num1 + this.num2;
+          break;
+        case '-':
+          this.equal = this.num1 - this.num2;
+          break;
+        case 'x':
+          this.equal = this.num1 * this.num2;
+          break;
+        case '/':
+          if (this.num2 !== 0) {
+            this.equal = this.num1 / this.num2;
+          } else {
+            // Se o denominador for zero, exibe uma mensagem de erro
+            this.equal = 'Erro: Divisão por zero';
+          }
+          break;
+        default:
+          this.equal = 'Operador inválido';
+      }
     } else {
-      this.equal = NaN; // Tratamento de divisão por zero
+      // Se um dos números não for fornecido, exibe uma mensagem de erro
+      this.equal = 'Por favor, forneça ambos os números';
     }
   }
 
-  onClickEqual(): void {
-    // Exibir uma mensagem quando o botão de igual é clicado
-    alert("Botão de igual foi clicado!");
-  }
 }
